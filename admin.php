586a8e9d9e122aecee710825dff67f71e482690b
quietmint <quietmint@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-09 22:13:44 +0000

    Added attribution warnings
    git-svn-id: https://plugins.svn.wordpress.org/speedy-smilies/trunk@133596 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin.php
+++ admin.php
@@ -1,14 +1,18 @@
 <?php
 
 // This should be automated...
-$q_smilies_sets = array('fugue', 'silk');
+$q_smilies_sets = array(
+	'fugue'	=> array(name => 'Fugue', author => 'Yusuke Kamiyamane', authoruri => 'http://pinvoke.com/'),
+	'silk'	=> array(name => 'Silk', author => 'Mark James', authoruri => 'http://www.famfamfam.com/lab/icons/silk/')
+);
 
 
 // Process form
 $updatedhtml = '';
 if ($_POST['speedy_smilies_set']) {
 	update_option('speedy_smilies_set', $_POST['speedy_smilies_set']);
-	$updatedhtml = '<div class="updated"><p><strong>Settings saved.</strong></p></div>';
+	$a = $q_smilies_sets[$_POST['speedy_smilies_set']];
+	$updatedhtml = '<div class="updated"><p><strong>Settings saved. Please update your blog&apos;s footer or &quot;About&quot; page to give credit to <a target="_blank" href="' . $a['authoruri'] . '">' . $a['author'] . '</a>.</strong></p></div>';
 }
 
 
@@ -17,9 +21,9 @@ $q_smilies_set = get_option('speedy_smilies_set');
 if (!$q_smilies_set) $q_smilies_set = 'fugue';
 
 $formhtml = '';
-foreach ($q_smilies_sets as $set) {
-	$formhtml .= '<input type="radio" name="speedy_smilies_set" value="' . $set . '"' . ($q_smilies_set == $set ? ' checked="checked"' : '') . ' /> <b>' . $set . '</b><br />';
-	$formhtml .= '<img src="' . WP_PLUGIN_URL . '/speedy-smilies/' . $set . '.png" alt="' . $set . '" title="' . $set . '" /><br /><br />';
+foreach ($q_smilies_sets as $set => $a) {
+	$formhtml .= '<input type="radio" name="speedy_smilies_set" value="' . $set . '"' . ($q_smilies_set == $set ? ' checked="checked"' : '') . ' /> <strong>' . $a['name'] . '</strong> by <a target="_blank" href="' . $a['authoruri'] . '">' . $a['author'] . '</a><br />';
+	$formhtml .= '<img src="' . WP_PLUGIN_URL . '/speedy-smilies/' . $set . '.png" alt="' . $a['name'] . '" title="' . $a['name']. '" /><br /><br />';
 }
 $formhtml .= '<p class="submit"><input type="submit" name="Submit" class="button-primary" value="Save Changes" /></p>';
 
@@ -28,6 +32,8 @@ print <<<HTML
 <div id="icon-themes" class="icon32"><br /></div>
 <h2>Speedy Smilies</h2><br />
 $updatedhtml
+<p>These icons have been provided free of charge under a <a target="_blank" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 license</a>.<br />
+Your blog must give credit to the respective graphic designer and include a link back to the icon author's page.</p>
 <form method="post" action="{$_SERVER['REQUEST_URI']}">
 $formhtml
 </form>
