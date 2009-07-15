bd2189a3082c1ccb75cc5b7ca9a56cc7bc5bc08d
quietmint <quietmint@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-15 15:39:41 +0000

    licensed under GNU GPL version 3
    git-svn-id: https://plugins.svn.wordpress.org/speedy-smilies/trunk@135429 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- speedy-smilies.php
+++ speedy-smilies.php
@@ -22,7 +22,7 @@ Author URI: http://quietmint.com/
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with Speedy Smilies.  If not, see <http://www.gnu.org/licenses/>.
 
 
 */
